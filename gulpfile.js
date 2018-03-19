c07e8d67686148a11b6949c3b87a8f18e988acbb
Colin <why101@msn.com>
2018-03-19 11:25:49 -0700

    added basic module files

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const fs = require('fs'),
+      gulp = require('gulp'),
+      clean = require('gulp-clean'),
+      include = require('gulp-include'),
+      rename = require('gulp-rename'),
+      zip = require('gulp-zip');
+
+gulp.task('clean', function() {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('build', ['clean'], function() {
+    gulp.src('index.js')
+        .pipe(include())
+        .on('error', console.log)
+        .pipe(gulp.dest('dist'));
+
+    gulp.src('partials/*.html')
+        .pipe(gulp.dest('dist/partials'));
+
+    gulp.src('docs/*.html')
+        .pipe(gulp.dest('dist/docs'));
+
+    gulp.src('module.json')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('release', function() {
+    let moduleInfo = JSON.parse(fs.readFileSync('module.json')),
+        moduleId = moduleInfo.id,
+        moduleVersion = moduleInfo.version,
+        zipFileName = `${moduleId}-v${moduleVersion}.zip`;
+
+    console.log(`Packaging ${zipFileName}`);
+
+    gulp.src('dist/**/*', { base: 'dist/'})
+        .pipe(rename((path) => path.dirname = `${moduleId}/${path.dirname}`))
+        .pipe(zip(zipFileName))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['build']);
