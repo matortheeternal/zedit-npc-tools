9410b605cd00972b41932d26f00df0c53ade1d6c
Colin <why101@msn.com>
2018-03-19 17:04:54 -0700

    now using xelib.WithEachHandle, added head part loading

--- src/npcHeadPartService.js
+++ src/npcHeadPartService.js
@@ -1,6 +1,7 @@
-ngapp.service('npcHeadPartService', function(settingsService, progressService) {
+ngapp.service('npcHeadPartService', function(settingsService, progressService, pluginInfoService) {
     let npcSettings = settingsService.settings.npcGeneration,
         validRaces = {},
+        ignoreKeys = {},
         maleHeadParts = {},
         femaleHeadParts = {};
 
@@ -21,11 +22,10 @@ ngapp.service('npcHeadPartService', function(settingsService, progressService) {
 
     let getEditorIDs = function(rec, path) {
         let editorIDs = [];
-        xelib.WithHandles(xelib.GetElements(rec, path), function(elements) {
-            elements.forEach(function(element) {
-                editorIDs.push(xelib.GetValue(element).split(' ')[0]);
-            });
+        xelib.WithEachHandle(xelib.GetElements(rec, path), function(element) {
+            editorIDs.push(xelib.GetValue(element).split(' ')[0]);
         });
+        return editorIds;
     };
 
     let getAllowedRaces = function(headPart) {
@@ -56,20 +56,43 @@ ngapp.service('npcHeadPartService', function(settingsService, progressService) {
         });
     };
 
+    let loadHeadPart = function(headPart, female, partType) {
+        let headParts = female ? femaleHeadParts : maleHeadParts;
+        if (!headParts.hasOwnProperty(partType)) headParts[partType] = [];
+        headParts[partType].push(headPart);
+    };
+
+    let getIgnoreKey = function(pnam) {
+        if (!ignoreKeys.hasOwnProperty(pnam))
+            ignoreKeys[pnam] = `ignoreBeth${pnam.toPascalCase()}`;
+        return ignoreKeys[pnam];
+    };
+
     // PUBLIC API
     this.loadHeadParts = function() {
-        // TODO
+        xelib.WithEachHandle(xelib.GetElements(), function(file) {
+            let headParts = xelib.GetRecords(file, 'HDPT'),
+                filename = xelib.Name(file),
+                isBethesdaFile = pluginInfoService.isBethesdaFile(filename);
+            xelib.WithEachHandle(headParts, function(headPart) {
+                if (!xelib.GetFlag(headPart, 'DATA - Flags', 'Playable')) return;
+                let male = xelib.GetFlag(headPart, 'DATA - Flags', 'Male'),
+                    pnam = xelib.GetValue(headPart, 'PNAM');
+                if (isBethesdaFile && npcSettings[getIgnoreKey(pnam)]) return;
+                loadHeadPart(headPart, !male, pnam);
+            });
+        });
     };
 
     this.generateHeadParts = function(npc, race, female) {
         xelib.RemoveElement(npc, 'Head Parts');
-        applyHeadPart(npc, race, female, 'hairs');
-        applyHeadPart(npc, race, female, 'eyes');
+        applyHeadPart(npc, race, female, 'Hair');
+        applyHeadPart(npc, race, female, 'Eyes');
         if (race !== 'Khajiit')
-            applyHeadPart(npc, race, female, 'brows');
+            applyHeadPart(npc, race, female, 'Eyebrows');
         if (race !== 'Argonian' && randomCheck(npcSettings.facialHairChance))
-            applyHeadPart(npc, race, female, 'facialHairs');
+            applyHeadPart(npc, race, female, 'Facial Hair');
         if (randomCheck(npcSettings.scarChance))
-            applyHeadPart(npc, race, female, 'scars');
+            applyHeadPart(npc, race, female, 'Scar');
     };
 });
