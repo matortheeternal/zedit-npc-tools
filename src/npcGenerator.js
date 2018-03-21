cd269fc428d34377850266d04d38ae95d1ab867b
Colin <why101@msn.com>
2018-03-20 23:28:36 -0700

    started work on npcGenerator service

--- src/npcGenerator.js
+++ src/npcGenerator.js
@@ -1,3 +1,79 @@
-ngapp.service('npcGenerator', function() {
-    // TODO
+ngapp.service('npcGenerator', function(settingsService, randomService, npcHeadPartService, npcNameGenerator, npcColorService, npcRaceService, npcVoiceService) {
+    let npcSettings = settingsService.settings.npcGeneration;
+
+    let randomInt = randomService.randomInt;
+    let weightedInt = randomService.weightedInt;
+
+    let newNpc = function(plugin) {
+        let npc = xelib.AddElement(plugin, 'NPC_\\.'),
+            race = npcRaceService.getRandomRace();
+        
+    };
+
+    let setWeight = function({npc}) {
+        if (npcSettings.skipWeight) return;
+        let weight = randomInt(20, 100);
+        SetIntValue(npc, 'NAM7', weight);
+    };
+
+    let setHeight = function({npc, raceHeight, female}) {
+        if (npcSettings.skipHeight) return;
+        let height = raceHeight[female ? 'Female' : 'Male'];
+        if (npcSettings.variableHeight) {
+            let {heightHigh, heightLow} = npcSettings;
+            height += randomInt(heightLow, heightHigh) / 100.0;
+        }
+        xelib.SetFloatValue(npc, 'NAM6', height);
+    };
+
+    let generateHeadParts = function({npc, race, female}) {
+        if (npcSettings.skipHeadParts) return;
+        npcHeadPartService.generateHeadParts(npc, race, female);
+    };
+
+    let generateMorphs = function({npc}) {
+        if (npcSettings.skipMorphs) return;
+        xelib.WithHandle(xelib.AddElement(npc, 'NAM9'), morphs => {
+            xelib.WithEachHandle(xelib.GetElements(morphs), morph => {
+                let value = weightedInt(-100, 100, 2);
+                xelib.SetFloatValue(morph, '', value / 100.0);
+            });
+        });
+    };
+
+    let generateFaceParts = function({npc}) {
+        if (npcSettings.skipFaceParts) return;
+        xelib.WithHandle(xelib.AddElement(npc, 'NAMA'), faceParts => {
+            xelib.SetIntValue(faceParts, 'Unknown', -1);
+            xelib.SetIntValue(faceParts, 'Nose', randomInt(1, 20));
+            xelib.SetIntValue(faceParts, 'Eyes', randomInt(1, 20));
+            xelib.SetIntValue(faceParts, 'Mouth', randomInt(1, 20));
+        });
+    };
+
+    let generateColors = function({npc, race, female}) {
+        npcColorService.generateColors(npc, race, female);
+    };
+
+    let generateName = function({npc, female}) {
+        if (npcSettings.skipNaming) return;
+        let lastName = randomService.randomCheck(npcSettings.lastNameChance);
+        npcNameGenerator.getName(npc, female, lastName)
+    };
+
+    let generateVoice = function({npc, race, female}) {
+        if (npcSettings.skipVoice) return;
+        npcVoiceService.generateVoice(npc, race, female);
+    };
+
+    // PUBLIC API
+    let generationSteps = [
+        setHeight, setWeight, generateHeadParts, generateMorphs,
+        generateFaceParts, generateColors, generateName, generateVoice
+    ];
+
+    this.generateNpc = function(plugin) {
+        let npcInfo = newNpc(plugin);
+        generationSteps.forEach(step => step(npcInfo));
+    };
 });
